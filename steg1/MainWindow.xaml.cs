de95fb866dc8153435d20d9b32f498cc5db1260f
andreyeyeye <andreyeyeye@DESKTOP-TGB9CN7>
2026-02-06 08:05:47 +0300

    Добавьте файлы проекта.

---
+++ steg1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using GPILabs;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace steg1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+        public void part1()
+        {
+            List<byte> data = l4.GetBytesFromBMP("C:\\Users\\andreyeyeye\\source\\repos\\ptzi1\\ptzi1\\1.bmp");
+            List<byte> data2 = l4.SelectBiteFromBMP(data, 7);
+            l4.SetBytesToBMP("C:\\Users\\andreyeyeye\\source\\repos\\ptzi1\\ptzi1\\2.bmp", data2);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            part1();
+        }
+    }
+}
---
+++ steg1/l4.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace GPILabs
+{
+    internal class l4
+    {
+        public static List<byte> SelectBiteFromBMP(List<byte> data, int byteIndex)
+        {
+            /*Debug.Write("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+*/
+
+            int colorsCount = BitConverter.ToInt32(data.GetRange(46, 50).ToArray(), 0);
+            Debug.WriteLine(colorsCount);
+            byte[] width = (data.GetRange(18, 4).ToArray());
+            byte[] height = (data.GetRange(22, 4).ToArray());
+            int heightInt = BitConverter.ToInt32(height, 0);
+            int widthInt = BitConverter.ToInt32(width, 0);
+            Debug.WriteLine($"{heightInt} * {widthInt} = {heightInt * widthInt} ||| {colorsCount} ||| {data.Count()} ||| {data.Count() - heightInt * widthInt}");
+            List<byte> data2 = data;
+
+            
+            int originalStride = (((data.Count - 54) / heightInt) % widthInt);
+            int currentIndex = 54;
+            for (int i = 0; i < heightInt; i++)
+            {
+                
+                for (int j = 0; j < widthInt; j++)
+                {
+                    int bitValue = (data[currentIndex] >> (7 - byteIndex)) & 1;
+                    if (bitValue == 1)
+                    {
+                        data2[currentIndex] = (byte)255;
+                    } else
+                    {
+                        data2[currentIndex] = (byte)0;
+                    }
+                    currentIndex++;
+                }
+                currentIndex += originalStride;
+            }
+
+
+
+            return data2;
+
+
+        }
+
+
+        public static List<byte> GetBytesFromBMP(string path)
+        {
+            List<byte> bytes = new List<byte>();
+
+            try
+            {
+                bytes = File.ReadAllBytes(path).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при чтении файла: " + ex.Message);
+                return null;
+            }
+
+
+            return bytes;
+        }
+
+        public static void SetBytesToBMP(string path, List<byte> bytes)
+        {
+            try
+            {
+                // Записываем массив байтов в файл
+                File.WriteAllBytes(path, bytes.ToArray());
+                Console.WriteLine("Файл успешно записан.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при записи файла: {ex.Message}");
+            }
+        }
+
+
+    }
+}
+
+
+
