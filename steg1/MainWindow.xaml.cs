be054eb515a0d794d924a1623beeecfe9b84968d
andreyeyeye <andreyeyeye@DESKTOP-TGB9CN7>
2026-02-07 07:19:21 +0300

    субрабочая 1 лаба

--- steg1/MainWindow.xaml.cs
+++ steg1/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.IO;
+using System.Diagnostics;
 namespace steg1
 {
     /// <summary>
@@ -18,6 +19,13 @@ namespace steg1
     public partial class MainWindow : Window
     {
         private string SelectedFolder = string.Empty;
+        private string BMPInPath = string.Empty;
+        private string TXTInPath = string.Empty;
+        private string BMPOutPath = string.Empty;
+        private string TXTOutPath = string.Empty;
+
+
+
         public MainWindow()
         {
 
@@ -87,5 +95,81 @@ namespace steg1
 			
 			}
 		}
-	}
+
+        private void getTXT_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TXTInPath = openFileDialog.FileName;
+            }
+            //Console.WriteLine(TXTInPath);
+        }
+
+        private void getBMP_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BMPInPath = openFileDialog.FileName;
+            }
+        }
+
+        private void CalcIntegration_Click(object sender, RoutedEventArgs e)
+        {
+            List<byte> data = l4.GetBytesFromBMP(BMPInPath);
+            List<byte> text = l4.GetBytesFromBMP(TXTInPath);
+            int bits = Convert.ToInt32(((ComboBoxItem)selectBit1.SelectedItem).Content);
+            List<byte> result = new List<byte>();
+            
+            result = l7.TextToBMP(data, text, bits);
+            
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                BMPOutPath = saveFileDialog.FileName;
+            }
+            Debug.WriteLine($"{result.Count}");
+            l4.SetBytesToBMP(BMPOutPath, result);
+        }
+
+        private void getBMP2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BMPInPath = openFileDialog.FileName;
+            }
+        }
+
+        private void CalcExtraction_Click(object sender, RoutedEventArgs e)
+        {
+            List<byte> data = l4.GetBytesFromBMP(BMPInPath);
+            int bits = Convert.ToInt32(((ComboBoxItem)selectBit2.SelectedItem).Content);
+            List<byte> result = new List<byte>();
+            
+            result = l7.BMPToText(data, bits);
+            
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                TXTOutPath = saveFileDialog.FileName;
+            }
+            l4.SetBytesToBMP(TXTOutPath, result);
+        }
+
+
+
+
+       
+
+
+    }
 }
---
+++ steg1/l7.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GPILabs
+{
+	internal class l7
+	{
+		public static List<byte> TextToBMP(List<byte> data, List<byte> text, int bitIndex)
+		{
+			List<byte> data2 = new List<byte>(data);
+			List<byte> result = new List<byte>(data.GetRange(0, 54));
+			List<bool> bitList = ByteListToBitList(text);
+			int width = BitConverter.ToInt32(data.GetRange(18, 4).ToArray(), 0);
+			int height = BitConverter.ToInt32(data.GetRange(22, 4).ToArray(), 0);
+			int currentIndex = 54;
+			int originalStride = ((data.Count-54) / height) % width;
+			int currentBit = 0;
+            Debug.WriteLine($"{height} * {width} = {height* width} ||| {data.Count()} ||| {data.Count() - height * width}");
+
+            for (int i = 0; i < height; i++)
+			{
+				for (int j = 0; j < width; j++)
+				{
+					byte tempByte = data[currentIndex];
+						
+					if(currentBit != bitList.Count - 1)
+					{
+								
+							
+						if (bitList[currentBit])
+						{
+							tempByte |= (byte)(1 << bitIndex);
+						}
+						else
+						{
+							tempByte &= (byte)~(1 << bitIndex); 
+						}
+						currentBit++;
+					}
+
+
+
+                    data2[currentIndex] = tempByte;
+
+					currentIndex++;
+				}
+				
+				
+			}
+
+
+			return result;
+		}
+
+
+		public static List<byte> BMPToText(List<byte> data, int bitIndex)
+		{
+			List<byte> result = new List<byte>(data.GetRange(0, 54));
+			List<bool> bitList = new List<bool>();
+			int width = BitConverter.ToInt32(data.GetRange(18, 4).ToArray(), 0);
+			int height = BitConverter.ToInt32(data.GetRange(22, 4).ToArray(), 0);
+			int currentIndex = 54;
+			int originalStride = ((data.Count - 54) / height) % width;
+			int currentBit = 0;
+
+			for (int i = 0; i < height; i++)
+			{
+				for (int j = 0; j < width; j++)
+				{
+					byte tempByte = data[currentIndex];
+					bool bitValue = (tempByte & (1 << bitIndex)) != 0;
+					bitList.Add(bitValue);
+					currentBit++;	
+					currentIndex++;
+					
+				}
+				currentIndex += originalStride;
+
+			}
+
+
+			return BitListToByteList(bitList);
+		}
+
+		public static List<bool> ByteListToBitList(List<byte> byteList)
+		{
+			BitArray bitArray = new BitArray(byteList.ToArray());
+			List<bool> bitList = new List<bool>();
+
+			foreach (bool bit in bitArray)
+			{
+				bitList.Add(bit);
+			}
+
+			return bitList;
+		}
+
+
+		public static List<byte> BitListToByteList(List<bool> bitList)
+		{
+			List<byte> byteList = new List<byte>();
+			int byteCount = (bitList.Count + 7) / 8; 
+
+			for (int i = 0; i < byteCount; i++)
+			{
+				byte b = 0;
+
+				
+				for (int j = 0; j < 8; j++)
+				{
+					int bitIndex = i * 8 + j;
+
+					
+					if (bitIndex < bitList.Count && bitList[bitIndex])
+					{
+						b |= (byte)(1 << j); 
+					}
+				}
+
+				byteList.Add(b); 
+			}
+
+			return byteList;
+		}
+
+
+
+	}
+}
