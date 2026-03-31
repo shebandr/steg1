ac753b5a2df1bb01c58a77fc77b17d172c4d87aa
Andreyeyeye <shebanoff.andrei@yandex.ru>
2026-04-01 01:42:31 +0700

    добавлен код Тардоса + мелкие изменения интерфейса

--- steg1/MainWindow.xaml.cs
+++ steg1/MainWindow.xaml.cs
@@ -27,63 +27,19 @@ namespace steg1
         private string TXTOutPath = string.Empty;
 		private string testBMPFolder = string.Empty;
 		private string refBMPPath = string.Empty;
-		private List<int> MPPZZero = new List<int>();
-		private List<int> MPPZPeak = new List<int>();
+
+		private string defaultKey = "DEFAULT KEY";
+
 		private int xiBlockSize = 32;
 		public MainWindow()
         {
 
             InitializeComponent();
-        }
 
-		public void part1()
-		{
-			
-			if (string.IsNullOrEmpty(SelectedFolder))
-				return;
+		
 
-			string[] bmpFiles = Directory.GetFiles(SelectedFolder, "*.bmp");
-			SelectedFolder = System.IO.Path.Combine(SelectedFolder, "output");
-			Directory.CreateDirectory(SelectedFolder);
-			foreach (string bmpPath in bmpFiles)
-			{
-				string fileNameWithoutExt =
-					System.IO.Path.GetFileNameWithoutExtension(bmpPath);
-
-				string outputDir = System.IO.Path.Combine(
-					SelectedFolder,
-					fileNameWithoutExt
-				);
-
-				Directory.CreateDirectory(outputDir);
-
-				List<byte> data = l4.GetBytesFromBMP(bmpPath);
-
-				for (int i = 0; i < 8; i++)
-				{
-					List<byte> result = l4.SelectByteFromBMP(data, i);
-
-					string outputPath = System.IO.Path.Combine(
-						outputDir,
-						$"{fileNameWithoutExt}_{i}.bmp"
-					);
-
-					l4.SetBytesToBMP(outputPath, result);
-				}
-
-				string outputPath2 = System.IO.Path.Combine(
-					outputDir,
-					$"{System.IO.Path.GetFileName(bmpPath)}"
-				);
-
-				l4.SetBytesToBMP(outputPath2, data);
-			}
 		}
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            part1();
-        }
 
 		private void SelectFolder_Click(object sender, RoutedEventArgs e)
 		{
@@ -104,18 +60,7 @@ namespace steg1
 			}
 		}
 
-        private void getTXT_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-
-            if (openFileDialog.ShowDialog() == true)
-            {
-                TXTInPath = openFileDialog.FileName;
-            }
-            //Console.WriteLine(TXTInPath);
-        }
-
+    
         private void getBMP_Click(object sender, RoutedEventArgs e)
         {
 
@@ -128,223 +73,8 @@ namespace steg1
             }
         }
 
-        private void CalcX2_Click(object sender, RoutedEventArgs e)
-        {
-			string BMPInPath2 = "";
+        
 
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
-            {
-                BMPInPath = openFileDialog.FileName;
-            }
-
-			openFileDialog = new OpenFileDialog();
-			if (openFileDialog.ShowDialog() == true)
-			{
-				BMPInPath2 = openFileDialog.FileName;
-			}
-
-			List<byte> data = l4.GetBytesFromBMP(BMPInPath);
-			List<byte> data2 = l4.GetBytesFromBMP(BMPInPath2);
-
-			List<List<double>> chi = Steganalysis.FullChiCalc(data, xiBlockSize);
-
-			List<List<double>> chi2 = Steganalysis.FullChiCalc(data2, xiBlockSize);
-			double sum = 0;
-			string output = string.Empty;
-			for (int i = 0; i < chi.Count; i++) 
-			{
-				for(int q = 0; q< chi[i].Count; q++)
-				{
-					output += (chi[i][q] - chi2[i][q]).ToString("F2");
-					output += " ";
-					sum += chi[i][q] - chi2[i][q];
-				}
-				output += "\n";
-			}
-			Debug.WriteLine(sum);
-            metricsOutput.Text = output;
-
-
-        }
-
-		private void calcRS_Click(object sender, RoutedEventArgs e)
-		{
-
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			if (openFileDialog.ShowDialog() == true)
-			{
-				BMPInPath = openFileDialog.FileName;
-			}
-
-			
-
-			List<byte> data = l4.GetBytesFromBMP(BMPInPath);
-
-			Steganalysis.RunRSAnalysis(data);
-
-			string output = string.Empty;
-		
-			metricsOutput.Text = output;
-
-		}
-
-		private void calcAUMP_Click(object sender, RoutedEventArgs e)
-		{
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			if (openFileDialog.ShowDialog() == true)
-			{
-				BMPInPath = openFileDialog.FileName;
-			}
-
-
-
-			List<byte> data = l4.GetBytesFromBMP(BMPInPath);
-			List<List<byte>> pixels = Steganalysis.SelectPixelsFromBMP(data);
-
-			byte[][] result = pixels.Select(row => row.ToArray()).ToArray();
-
-
-			double beta_aump = AUMP.Aump(result, m: 16, d: 2);
-			Debug.WriteLine($"AUMP beta: {beta_aump}");
-
-
-			
-		}
-
-
-		private void calsXi2Mass_Click(object sender, RoutedEventArgs e)
-		{
-
-
-
-            Dictionary<string, List<List<double>>> allResults = new Dictionary<string, List<List<double>>>();
-            string[] bmpFiles = Directory.GetFiles(testBMPFolder, "*.bmp");
-            foreach (var bmpFile in bmpFiles)
-            {
-                try
-                {
-                    List<byte> data = l4.GetBytesFromBMP(bmpFile);
-
-                    List<List<double>> chi = Steganalysis.FullChiCalc(data, xiBlockSize);
-
-                    allResults.Add(Path.GetFileNameWithoutExtension(bmpFile), chi);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Ошибка при обработке файла: {bmpFile}");
-                    Console.WriteLine($"Сообщение: {ex.Message}");
-                }
-            }
-            StringBuilder sb = new StringBuilder();
-
-            foreach (var pair in allResults)
-            {
-				sb.Append(pair.Key);
-				sb.AppendLine();
-				foreach(var pair2 in pair.Value)
-				{
-                    string temp = "";
-					foreach(var pair3 in pair2)
-					{
-						string t = pair3.ToString("F2");
-
-                        temp += $"{t}\t";
-					}
-
-
-					sb.Append(temp);
-                }
-
-				sb.AppendLine();
-            }
-
-            string resultString = sb.ToString();
-            string outputPath = Path.Combine(testBMPFolder, "resultsXi.tsv");
-            File.WriteAllText(outputPath, resultString, Encoding.UTF8);
-			metricsOutput.Text = resultString;
-        }
-
-		private void calcRSMass_Click(object sender, RoutedEventArgs e)
-		{
-			Dictionary<string, double> allResults = new Dictionary<string, double>();
-			string[] bmpFiles = Directory.GetFiles(testBMPFolder, "*.bmp");
-			foreach (var bmpFile in bmpFiles)
-			{
-				try
-				{
-					List<byte> data = l4.GetBytesFromBMP(bmpFile);
-					double rsResult = Steganalysis.RunRSAnalysis(data);
-
-					allResults.Add(Path.GetFileNameWithoutExtension(bmpFile), rsResult);
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine($"Ошибка при обработке файла: {bmpFile}");
-					Console.WriteLine($"Сообщение: {ex.Message}");
-				}
-			}
-			StringBuilder sb = new StringBuilder();
-
-			foreach (var pair in allResults)
-			{
-				string value = pair.Value.ToString(new CultureInfo("ru-RU"));
-
-				sb.Append(pair.Key)
-				  .Append('\t')
-				  .Append(value)
-				  .AppendLine();
-			}
-
-			string resultString = sb.ToString(); 
-			string outputPath = Path.Combine(testBMPFolder, "resultsRS.tsv");
-			File.WriteAllText(outputPath, resultString, Encoding.UTF8);
-            metricsOutput.Text = resultString;
-        }
-
-		private void calcAUMPMass_Click(object sender, RoutedEventArgs e)
-		{
-			Dictionary<string, double> allResults = new Dictionary<string, double>();
-			string[] bmpFiles = Directory.GetFiles(testBMPFolder, "*.bmp");
-			foreach (var bmpFile in bmpFiles)
-			{
-				try
-				{
-					List<byte> data = l4.GetBytesFromBMP(bmpFile);
-					List<List<byte>> pixels = Steganalysis.SelectPixelsFromBMP(data);
-
-					byte[][] result = pixels.Select(row => row.ToArray()).ToArray();
-
-
-					double beta_aump = AUMP.Aump(result, m: 16, d: 2);
-					allResults.Add(Path.GetFileNameWithoutExtension(bmpFile), beta_aump);
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine($"Ошибка при обработке файла: {bmpFile}");
-					Console.WriteLine($"Сообщение: {ex.Message}");
-				}
-			}
-
-			StringBuilder sb = new StringBuilder();
-			foreach (var pair in allResults)
-			{
-				string value = pair.Value.ToString(new CultureInfo("ru-RU"));
-
-				sb.Append(pair.Key)
-				  .Append('\t')
-				  .Append(value)
-				  .AppendLine();
-			}
-
-			string resultString = sb.ToString();
-			string outputPath = Path.Combine(testBMPFolder, "resultsAUMP.tsv");
-			File.WriteAllText(outputPath, resultString, Encoding.UTF8);
-            metricsOutput.Text = resultString;
-        }
-
-		
-		
 
 
 
@@ -411,56 +141,49 @@ namespace steg1
 			metricsOutput.Text = outputMetrics;
 		}
 
-		private void getBMPFolder_Click(object sender, RoutedEventArgs e)
+		
+
+		
+
+		private void CalcIntegration_Click(object sender, RoutedEventArgs e)
 		{
+			string key = keyField.Text;
+			if (key == "")
+			{
+				key = defaultKey;
+			}
+			int origUsersCout = Int32.Parse(origUsersCountField.Text);
 
+
+
+			Tardos.BuildCode(10, 2, 0.1);
+
+			// 2. Получение инфы
+			var info = Tardos.GetInfo();
+			Debug.WriteLine($"Users: {info.users}, Length: {info.codeLength}, c: {info.collisionSize}");
+
+			// 3. Симуляция атаки
+			int[] attackers = { 2, 5, 7, 9 };
+
+			var y = Tardos.Collide(attackers);
+
+			// 4. Детект
+			var result = Tardos.Detect(y);
+
+			// 5. Вывод топ подозреваемых
+			Debug.WriteLine("Top suspects:");
+			for (int i = 0; i < 10; i++)
+			{
+				Debug.WriteLine($"User {result[i].user}, score = {result[i].score:F4}");
+			}
 		}
 
-        private void calcMetrics2_Click(object sender, RoutedEventArgs e)
-        {
-            string[] bmpFiles = Directory.GetFiles(testBMPFolder, "*.bmp");
-            string folder = Path.GetDirectoryName(testBMPFolder)!;
-            string csvPath = Path.Combine(
-                folder,
-                Path.GetFileNameWithoutExtension(testBMPFolder) + ".csv"
-            );
-
-            if (!File.Exists(csvPath))
-            {
-                var header = new StringBuilder("Image");
-                for (int i = 1; i < 8; i++) 
-				{
-					header.Append($";{i} MAX");
-					header.Append($";{i} FREE");
-				}
-                header.AppendLine();
-
-                File.WriteAllText(csvPath, header.ToString());
-            }
-
-            foreach (var bmpFile in bmpFiles)
-            {
-                var line = new StringBuilder();
-                List<int> hist = ImageMetrics.BuildHistogram(bmpFile);
-
-                line.Append(Path.GetFileNameWithoutExtension(bmpFile));
-
-                for (int i = 1; i < 8; i++)
-                {
-                    (int maxCapacity, int usedCapacity) = HistogramShifting.CalcSpace(hist, i);
-
-                    line.Append($";{maxCapacity}");
-                    line.Append($";{usedCapacity}");
-                }
-
-                line.AppendLine();
-                File.AppendAllText(csvPath, line.ToString());
-            }
-		}
 
 		private void CalcExtraction_Click(object sender, RoutedEventArgs e)
 		{
 
 		}
+
+
 	}
 }
---
+++ steg1/Tardos.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+
+namespace steg1
+{
+	public static class Tardos
+	{
+		private static int CodeLength;
+		private static int N;
+		private static char[][] Code;
+		private static int c;
+		private static double[] P;
+		private static double k;
+		private static Random rnd = new Random();
+
+
+		private static double GetRND(double a, double b)
+		{
+			return rnd.NextDouble() * (b - a) + a;
+		}
+
+		private static double Sqr(double x)
+		{
+			return x * x;
+		}
+
+		private static char GenOne(double prob)
+		{
+			return (rnd.NextDouble() < prob) ? '1' : '0';
+		}
+
+		private static double CalcScore(int i, char[] y)
+		{
+			double score = 0;
+			double U;
+
+			for (int j = 0; j < CodeLength; j++)
+			{
+				if (Code[i][j] == '1')
+					U = Math.Sqrt((1 - P[j]) / P[j]);
+				else
+					U = -Math.Sqrt(P[j] / (1 - P[j]));
+
+				score += (y[j] - '0') * U;
+			}
+
+			return score;
+		}
+
+
+		public static void BuildCode(int CNT, double c0, double eps)
+		{
+			N = CNT;
+			k = Math.Ceiling(Math.Log(1 / eps));
+			c = (int)c0;
+			CodeLength = (int)(100 * c * c * k);
+
+			P = new double[CodeLength];
+			Code = new char[N][];
+
+			for (int i = 0; i < N; i++)
+				Code[i] = new char[CodeLength];
+
+			double t = 1.0 / (300.0 * c);
+			double t_ = Math.Asin(Math.Sqrt(t));
+
+			for (int j = 0; j < CodeLength; j++)
+			{
+				double r = GetRND(t_, Math.PI / 2 - t_);
+				P[j] = Sqr(Math.Sin(r));
+			}
+
+			for (int i = 0; i < N; i++)
+			{
+				for (int j = 0; j < CodeLength; j++)
+				{
+					Code[i][j] = GenOne(P[j]);
+				}
+			}
+		}
+
+		public static char[][] GetCodeTable()
+		{
+			return Code;
+		}
+
+		public static char[] GetCodeWord(int i)
+		{
+			return Code[i];
+		}
+
+		public static (int users, int codeLength, int collisionSize) GetInfo()
+		{
+			return (N, CodeLength, c);
+		}
+
+		public static char[] Collide(int[] attackers)
+		{
+			char[] y = new char[CodeLength];
+
+			for (int j = 0; j < CodeLength; j++)
+			{
+				char first = Code[attackers[0]][j];
+				bool allEqual = true;
+
+				for (int k = 1; k < attackers.Length; k++)
+				{
+					if (Code[attackers[k]][j] != first)
+					{
+						allEqual = false;
+						break;
+					}
+				}
+
+				if (allEqual)
+				{
+					y[j] = first;
+				}
+				else
+				{
+					y[j] = (char)('0' + rnd.Next(2));
+				}
+			}
+
+			return y;
+		}
+
+		public static (int user, double score)[] Detect(char[] y)
+		{
+			var result = new (int user, double score)[N];
+
+			for (int i = 0; i < N; i++)
+			{
+				result[i] = (i, CalcScore(i, y));
+			}
+
+			return result
+				.OrderByDescending(x => x.score)
+				.ToArray();
+		}
+	}
+}
